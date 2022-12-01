0ad20d59084b0a0e890d692e1aa1b527c62fda82
lewis-cressey <63366182+lewis-cressey@users.noreply.github.com>
2022-12-01 10:49:27 +0100

    Simplified event handling for buttons.

--- main.py
+++ main.py
@@ -9,62 +9,75 @@ def show_popup(text):
     page.stderr.textContent = text
     page.popup_layer.setAttribute("class", "")
 
+def show_traceback():
+    show_popup(traceback.format_exc())
+    
 def hide_popup(*args):
     page.popup_layer.setAttribute("class", "hidden")
 
-def client_select(selector, allow_none = False):
+def get_stdout():
+    element = page.inner_document.getElementById("stdout")
+    if element is None:
+        element = page.inner_document.createElement("div")
+        element.id = "stdout"
+        page.root_element.querySelector("body").append(element)
+    return element
+
+def client_select(selector, raise_on_fail = True):
+    if not isinstance(selector, str): return selector
     element = page.inner_document.getElementById(selector)
     if element is not None: return element
     element = page.root_element.querySelector(selector)      
     if element is not None: return element
-    if allow_none: return None
-    raise RuntimeError(f"No such element as {selector}")
+    if raise_on_fail: raise RuntimeError(f"No such element as {selector}")
+    return None
 
-def client_decorator_click(selector):
-    def decorator(function):
-        client_select(selector).bind("click", function)
-        return function
-    return decorator
-
-def client_on(selector, event, function):
-    element = client_select(selector)
-    element.bind(event, function)
+def client_clear(selector = None):
+    element = client_select(selector or get_stdout())
+    element.innerHTML = ""
 
 def client_input(selector):
     control = client_select(selector)
-    return control.value
+    if hasattr(control, "value"): return control.value
+    return control.textContent
 
 def client_print(*args, **kwargs):
     output = io.StringIO()
     print(*args, file=output, **kwargs)
     
-    element_id = kwargs.get("to", "#stdout")
-    element = client_select(element_id, allow_none = True)
-    if element is None:
-        element = page.inner_document.createElement("div")
-        element.id = "stdout"
-        page.root_element.querySelector("body").append(element)
-    element.textContent += output.getvalue()
+    element_id = kwargs.get("to")
+    element = client_select(element_id or get_stdout())
+    if hasattr(element, "value"): element.value = output.getvalue()
+    else: element.innerHTML += output.getvalue()
+
+def client_button_click(event, global_vars):
+    button_id = event.target.id
+    method_name = f"click_{button_id}"
+    callback = global_vars.get(method_name)
+    if callback is not None:
+        try: callback()
+        except Exception as e: show_traceback()
 
 def run_script(text):
     global_vars = globals().copy()    
     global_vars["select"] = client_select
+    global_vars["clear"] = client_clear
     global_vars["input"] = client_input
     global_vars["print"] = client_print
-    global_vars["on"] = client_on
-    global_vars["click"] = client_decorator_click
     ids = SimpleNamespace()
     global_vars["page"] = ids
 
     ids.body = page.root_element.querySelector("body")
     for element in page.root_element.querySelectorAll("[id]"):
         setattr(ids, element.id, element)
+        if element.tagName == "BUTTON":
+            def callback(event): return client_button_click(event, global_vars)
+            element.bind("click", callback)
     
     try:
         exec(text, global_vars)
     except Exception as e:
-        text = traceback.format_exc()
-        show_popup(text)
+        show_traceback()
     
 def main():
     page.inner_document = document["user-page"].contentWindow.document
